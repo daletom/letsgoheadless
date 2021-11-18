f71e02543dd4dec360a3f52002818cf574658fae
Tom Dale <thomas.dale2@gmail.com>
2021-11-18 15:14:38 -0800

    add twitter:image

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,9 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'This is a informational and tutorial website about Headless Web Architecture.  Learn to make your own headless website by viewing over 20 simple tutorials accompanied with videos.  Each tutorial is a combination of using either Gatsby, Next.js, or Nuxt.js as a front-end framework along with many headless CMS examples.  ' },
-      { hid: 'og:image', property: 'og:image', content: 'https://tom.imgix.net/letsgoheadless_logo_mobile.png?ixlib=vue-2.9.0&auto=format%2Ccompress&fit=crop&ar=1.8%3A1&bg=262261&w=799'}
+      { hid: 'og:image', property: 'og:image', content: 'https://tom.imgix.net/letsgoheadless_logo_mobile.png?ixlib=vue-2.9.0&auto=format%2Ccompress&fit=crop&ar=1.8%3A1&bg=262261&w=799'},
+      { hid: 'twitter:description', name: 'twitter:description', content: 'This is a informational and tutorial website about Headless Web Architecture.  Learn to make your own headless website by viewing over 20 simple tutorials accompanied with videos.  Each tutorial is a combination of using either Gatsby, Next.js, or Nuxt.js as a front-end framework along with many headless CMS examples.  ' },
+      { hid: 'twitter:image', property: 'twitter:image', content: 'https://tom.imgix.net/letsgoheadless_logo_mobile.png?ixlib=vue-2.9.0&auto=format%2Ccompress&fit=fill&ar=1%3A1&bg=262261&w=400&h=400'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
